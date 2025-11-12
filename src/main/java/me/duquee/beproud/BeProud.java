bb7b04c9020fccf6b36fbcc0368b8a10b456703e
iLofiz <iloaf9820@gmail.com>
2025-11-12 22:20:33 +0000

    Initial Commit

---
+++ src/main/java/me/duquee/beproud/BeProud.java
@@ -0,0 +1,11 @@
+package me.duquee.beproud;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BeProud implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
